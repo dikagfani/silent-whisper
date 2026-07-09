890095efcd64f4e078883f4853f0891894310884
dikagfani <ardikaghathfani@gmail.com>
2026-07-09 18:57:53 +0700

    add Navbar Components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,19 +11,19 @@ export default function Navbar() {
       </div>
 
       <div className="hidden md:flex items-center gap-8">
-        <Link href="/beranda" className="text-sm font-medium text-gray-600 hover:text-blue-600 transition-colors">
+        <Link href="/" className="text-sm font-medium text-gray-400 hover:text-foreground transition-colors">
           Beranda
         </Link>
-        <Link href="/galeri" className="text-sm font-medium text-gray-600 hover:text-blue-600 transition-colors">
+        <Link href="/galeri" className="text-sm font-medium text-gray-400 hover:text-foreground transition-colors">
           Galeri
         </Link>
-        <Link href="/artikel" className="text-sm font-medium text-gray-600 hover:text-blue-600 transition-colors">
+        <Link href="/artikel" className="text-sm font-medium text-gray-400 hover:text-foreground transition-colors">
           Artikel
         </Link>
-        <Link href="/tentang-kami" className="text-sm font-medium text-gray-600 hover:text-blue-600 transition-colors">
+        <Link href="/tentang-kami" className="text-sm font-medium text-gray-400 hover:text-foreground transition-colors">
           Tentang Kami
         </Link>
-        <Link href="/kontak" className="text-sm font-medium text-gray-600 hover:text-blue-600 transition-colors">
+        <Link href="/kontak" className="text-sm font-medium text-gray-400 hover:text-foreground transition-colors">
           Kontak
         </Link>
       </div>
@@ -31,7 +31,7 @@ export default function Navbar() {
       <div className="flex items-center">
         <Link 
           href="https://api.whatsapp.com/send?phone=6281394818985&text=Halo%20min,%20mau%20tanya%20showcase%20Silent%20Whisper!" 
-          className="bg-background border border-gray-500 px-4 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-[#FD5A37] transition-all active:scale-98"
+          className="bg-background border-2 border-gray-400 px-4 py-2.5 text-sm font-medium text-white shadow-xs hover:bg-[#FD5A37] hover:border-[#FD5A37] transition-all"
         >
           Hubungi Kami
         </Link>
